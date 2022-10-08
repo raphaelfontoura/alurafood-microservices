6f54e52ea87067651f9b2cfe9fea80090fc912c2
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-10-08 18:41:38 -0300

    Remove @NotNull and @NotBlank

--- pagamentos/src/main/java/br/com/alurafood/pagamentos/model/Pagamento.java
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/model/Pagamento.java
@@ -23,32 +23,26 @@ public class Pagamento {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull
     @Positive
     private BigDecimal valor;
 
-    @NotBlank
     @Size(max = 100)
     private String nome;
 
-    @NotBlank
     @Size(max = 19)
     private String numero;
 
-    @NotBlank
     @Size(max = 7)
     private String expiracao;
 
-    @NotBlank
     @Size(max = 3, min = 3)
     private String codigo;
 
-    @NotNull
     @Enumerated(EnumType.STRING)
     private Status status;
-    @NotNull
+
     private Long pedidoId;
-    @NotNull
+
     private Long formaDePagamentoId;
 
 }
