778461bebe49f49ff13a7552b2cb4ce9f6a7d4cd
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-11-05 00:05:31 -0300

    Add feing client to sync services

--- pagamentos/src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
@@ -52,4 +52,9 @@ public class PagamentoController {
         return ResponseEntity.noContent().build();
     }
 
+    @PatchMapping("/{id}/confirmar")
+    public void confirmarPagamento(@PathVariable @NotNull Long id) {
+        service.confirmarPagamento(id);
+    }
+
 }
