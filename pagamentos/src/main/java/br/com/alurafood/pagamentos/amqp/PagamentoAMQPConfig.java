e69a00a85c8d22fe2ea56b2759df04f95585fb9c
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-11-15 16:47:51 -0300

    Create PagamentoAMQPConfig

---
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/amqp/PagamentoAMQPConfig.java
@@ -0,0 +1,30 @@
+package br.com.alurafood.pagamentos.amqp;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PagamentoAMQPConfig {
+
+    @Bean
+    public Queue createQueue() {
+//        return new Queue("pagamento.concluido", false);
+        return QueueBuilder.nonDurable("pagamento.concluido").build();
+    }
+
+    @Bean
+    public RabbitAdmin createRabbitAdmin(ConnectionFactory conn) {
+        return new RabbitAdmin(conn);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> startAdmin(RabbitAdmin rabbitAdmin) {
+        return event -> rabbitAdmin.initialize();
+    }
+}
