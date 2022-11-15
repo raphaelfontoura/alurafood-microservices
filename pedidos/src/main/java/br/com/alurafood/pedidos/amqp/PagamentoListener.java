9998d9cc4946c5cee139b6e91ede8ca8a9481be7
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-11-15 19:38:06 -0300

    Refactor PagamentoListener. Implement Jackson2JsonMessageConverter to RabbitTemplate in pedidos-ms

--- pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
+++ pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
@@ -1,6 +1,6 @@
 package br.com.alurafood.pedidos.amqp;
 
-import org.springframework.amqp.core.Message;
+import br.com.alurafood.pedidos.dto.PagamentoDto;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -8,7 +8,14 @@ import org.springframework.stereotype.Component;
 public class PagamentoListener {
 
     @RabbitListener(queues = "pagamento.concluido")
-    public void readMessage(Message message) {
-        System.out.println("Recebido a mensagem " + message.toString());
+    public void readMessage(PagamentoDto pagamento) {
+        String message = """
+                Dados do pagamento: %s
+                Número do pedido: %s
+                Valor R$: %s
+                Status: %s
+                """.formatted(pagamento.getId(), pagamento.getPedidoId(), pagamento.getValor(), pagamento.getStatus());
+
+        System.out.println("Recebido a mensagem " + message);
     }
 }
